fbb51199ce8d00c26c7fd546e5d5c5616b5e8fc6
Max ray <Maks_rio2003@mail.ru>
2023-06-18 19:11:41 +0200

    Hero section v1

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+
+    $(".no-transition").removeClass("no-transition");
+    $(".hero__video_wrapper").addClass("active");
+    $(".rise-by-line-animation").addClass("active");
+
+});
