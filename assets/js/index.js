87636700ff85ce17a062db743487fcf5e871f7be
Max ray <Maks_rio2003@mail.ru>
2023-06-19 16:45:30 +0200

    about

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,10 +9,10 @@ $(function () {
   function heroAnimation(isActive) {
     if (isActive) {
       $(".hero__video_wrapper").addClass("active");
-      $(".rise-by-line-animation").addClass("active");
+      $(".hero__title.rise-by-line-animation").addClass("active");
     } else {
       $(".hero__video_wrapper").removeClass("active");
-      $(".rise-by-line-animation").removeClass("active");
+      $(".hero__title.rise-by-line-animation").removeClass("active");
     }
   }
   // Hide/show hero section after scroll
@@ -30,16 +30,76 @@ $(function () {
     let $this = $(this);
     $this.lettering();
 
-    gsap.from($this.find('span'), {
-        alpha: .4,
-        stagger: .1,
-        scrollTrigger: {
-            trigger: $this,
-            start: 'top 60%',
-            end: 'bottom 40%',
-            scrub: 1,
-        }
-    })
-
+    gsap.from($this.find("span"), {
+      alpha: 0.2,
+      stagger: 0.1,
+      scrollTrigger: {
+        trigger: $this,
+        start: "top 60%",
+        end: "bottom 40%",
+        scrub: 1,
+      },
+    });
   });
+
+  //rise-by-line-animation
+  $(".rise-by-line-animation:not(.animation-by-class)").each(function () {
+    let $this = $(this);
+
+    gsap.from($this.find("span"), {
+      yPercent: 150,
+      rotate: 5,
+      stagger: 0.3,
+      scrollTrigger: {
+        trigger: $this,
+        start: "top 70%",
+        end: "bottom 70%",
+        scrub: 0.4,
+      },
+    });
+  });
+
+  //about__background_wrapper
+
+  let backgroundImagesTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 0.1,
+    },
+  });
+
+  backgroundImagesTimeline.fromTo(
+    ".about__background_wrapper div",
+    {
+      yPercent: 120,
+    },
+    {
+      yPercent: -120,
+      ease: "linear",
+    }
+  );
+  let side;
+  $(".about__background_wrapper div > *").each(function () {
+    if (side == undefined) {
+        side = getRandomInt(1);
+    }
+    backgroundImagesTimeline.fromTo(
+      this,
+      {
+        yPercent: getRandomInt(25),
+        rotate: side ? getRandomInt(15) : -getRandomInt(15),
+      },
+      {
+        yPercent: -getRandomInt(40),
+        rotate: side ? -getRandomInt(15) : getRandomInt(15),
+      },
+      "<"
+    );
+    side = !side;
+  });
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 });
