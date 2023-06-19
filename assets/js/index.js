947d6da79b779d70f957f266584269d0771c6325
Max ray <Maks_rio2003@mail.ru>
2023-06-19 12:05:43 +0200

    intro

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,45 @@
 $(function () {
+  gsap.registerPlugin(ScrollTrigger);
 
-    $(".no-transition").removeClass("no-transition");
-    $(".hero__video_wrapper").addClass("active");
-    $(".rise-by-line-animation").addClass("active");
+  //Fix for onload issue
+  $(".no-transition").removeClass("no-transition");
 
+  //Show hero section on load
+  heroAnimation(true);
+  function heroAnimation(isActive) {
+    if (isActive) {
+      $(".hero__video_wrapper").addClass("active");
+      $(".rise-by-line-animation").addClass("active");
+    } else {
+      $(".hero__video_wrapper").removeClass("active");
+      $(".rise-by-line-animation").removeClass("active");
+    }
+  }
+  // Hide/show hero section after scroll
+  ScrollTrigger.create({
+    trigger: ".hero",
+    start: "-100 top",
+    end: "bottom 70%",
+    onToggle: (self) => {
+      heroAnimation(self.isActive);
+    },
+  });
+
+  //Lettering animation
+  $(".letters-animation").each(function () {
+    let $this = $(this);
+    $this.lettering();
+
+    gsap.from($this.find('span'), {
+        alpha: .4,
+        stagger: .1,
+        scrollTrigger: {
+            trigger: $this,
+            start: 'top 60%',
+            end: 'bottom 40%',
+            scrub: 1,
+        }
+    })
+
+  });
 });
