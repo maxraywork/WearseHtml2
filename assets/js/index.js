3ee381d80298e0b4380da704df8414e3a6ef35ed
Max ray <Maks_rio2003@mail.ru>
2023-06-25 00:44:04 +0200

    mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 $(function () {
   gsap.registerPlugin(ScrollTrigger);
 
+  isDesktop = window.innerWidth > 1024;
+
   //Fix for onload issue
   $(".no-transition").removeClass("no-transition");
 
@@ -43,21 +45,42 @@ $(function () {
   });
 
   //rise-by-line-animation
-  $(".rise-by-line-animation:not(.animation-by-class)").each(function () {
-    let $this = $(this);
+  if (isDesktop) {
+    $(".rise-by-line-animation:not(.animation-by-class)").each(function () {
+      let $this = $(this);
 
-    gsap.from($this.find("span"), {
-      yPercent: 150,
-      rotate: 5,
-      stagger: 0.3,
-      scrollTrigger: {
-        trigger: $this,
-        start: "top 70%",
-        end: "bottom 70%",
-        scrub: 0.4,
-      },
+      gsap.from($this.find("span"), {
+        yPercent: 150,
+        rotate: 5,
+        stagger: 0.3,
+        scrollTrigger: {
+          trigger: $this,
+          start: "top 70%",
+          end: "bottom 70%",
+          scrub: 0.4,
+        },
+      });
     });
-  });
+  } else {
+    //About mobile
+    $(".about__text div span").each(function () {
+      let $this = $(this);
+      $this.lettering("words");
+    });
+    $(".about__text").each(function () {
+      gsap.from($(this).find(".word"), {
+        yPercent: 130,
+        rotate: 4,
+        stagger: 0.3,
+        scrollTrigger: {
+          trigger: this,
+          start: "top 90%",
+          end: "bottom 50%",
+          scrub: 0.5,
+        },
+      });
+    });
+  }
 
   //about__background_wrapper
   let backgroundImagesTimeline = gsap.timeline({
@@ -68,16 +91,27 @@ $(function () {
       scrub: 0.1,
     },
   });
-
-  backgroundImagesTimeline.fromTo(
-    ".about__background_wrapper div",
-    {
-      yPercent: 10,
-    },
-    {
-      yPercent: -60,
-    }
-  );
+  if (isDesktop) {
+    backgroundImagesTimeline.fromTo(
+      ".about__background_wrapper div",
+      {
+        yPercent: 10,
+      },
+      {
+        yPercent: -60,
+      }
+    );
+  } else {
+    backgroundImagesTimeline.fromTo(
+      ".about__background_wrapper div",
+      {
+        yPercent: 10,
+      },
+      {
+        yPercent: -10,
+      }
+    );
+  }
   let side;
   $(".about__background_wrapper div > *").each(function () {
     if (side == undefined) {
@@ -195,7 +229,10 @@ $(function () {
       ease: "linear",
       scrollTrigger: {
         trigger: ".contact .title",
-        start: () => "bottom+=" + $('.contact').css('paddingBottom').split('px')[0] + " bottom",
+        start: () =>
+          "bottom+=" +
+          $(".contact").css("paddingBottom").split("px")[0] +
+          " bottom",
         end: () => "bottom bottom",
         endTrigger: ".contact",
         markers: false,
@@ -203,53 +240,57 @@ $(function () {
       },
     });
 
-
     let timelineSmallBubble = gsap.timeline({
       scrollTrigger: {
         trigger: ".contact .title",
-        start: 'bottom 70%',
+        start: "bottom 70%",
         end: "bottom bottom",
         endTrigger: "html",
         scrub: 1,
-        markers: false
-      }
-    })
-    timelineSmallBubble.from(".contact__text__bubble", {
-      scale: 0
-    })
-    .from(".contact__text > div > span", {
-      yPercent: 140,
-      rotate: 3,
-      stagger: 1
-    })
-    .from(".contact__text__bubble_text span", {
-      yPercent: 140,
-      rotate: 4,
-      stagger: .5
-    })
-    .from('.contact__info_background', {
-      scale: 0,
-    }, "<")
-    .from(".contact__info__text a", {
-      yPercent: 140,
-      rotate: 4,
-      stagger: .5,
-      scrollTrigger: {
-        trigger: ".contact__info",
-        start: '80% bottom',
-        end: "bottom bottom",
-        scrub: .5,
-        markers: false
-      }
-    }, "<")
-    .from(".contact__info__social div a", {
-      yPercent: 140,
-      rotate: 3,
-      stagger: .5
+        markers: false,
+      },
     });
-
-
-    
-
+    timelineSmallBubble
+      .from(".contact__text__bubble", {
+        scale: 0,
+      })
+      .from(".contact__text > div > span", {
+        yPercent: 140,
+        rotate: 3,
+        stagger: 1,
+      })
+      .from(".contact__text__bubble_text span", {
+        yPercent: 140,
+        rotate: 4,
+        stagger: 0.5,
+      })
+      .from(
+        ".contact__info_background",
+        {
+          scale: 0,
+        },
+        "<"
+      )
+      .from(
+        ".contact__info__text a",
+        {
+          yPercent: 140,
+          rotate: 4,
+          stagger: 0.5,
+          scrollTrigger: {
+            trigger: ".contact__info",
+            start: "80% bottom",
+            end: "bottom bottom",
+            scrub: 0.5,
+            markers: false,
+          },
+        },
+        "<"
+      )
+      .from(".contact__info__social div a", {
+        yPercent: 140,
+        rotate: 3,
+        stagger: 0.5,
+      });
   });
 });
